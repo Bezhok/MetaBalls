a492a103322bbfc85eaf937552896c34b25c10a2
Nick <nickbezhok@gmail.com>
2020-03-01 23:01:55 +0300

    Refactoring

--- Assets/src/Circle.cs
+++ Assets/src/Circle.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace src
 {
@@ -9,54 +7,55 @@ namespace src
         private Mesh _mesh;
 
         public Vector3 speed;
-        
+
         public float Radius { get; set; }
 
         public Color FillColor { get; set; }
         public Vector3 TexturePosition { get; set; }
+
         private void Awake()
         {
             _mesh = GetComponent<MeshFilter>().mesh;
             FillColor = Color.white;
         }
 
-        public void Create(float radius, int quality = 40)
+        public void CreateMesh(float radius, int quality = 40)
         {
             Radius = radius;
-            
+
             var center = Vector2.zero;
-            var stepCount = (int)(quality * Mathf.Max(1, radius));
-            
-            var colors = new Color[4*(stepCount+1)];
-            var vertices = new Vector3[4*(stepCount+1)];
+            var stepCount = (int) (quality * Mathf.Max(1, radius));
+
+            var colors = new Color[4 * (stepCount + 1)];
+            var vertices = new Vector3[4 * (stepCount + 1)];
             int[] eboTemplate = {0, 1, 2, 1, 3, 2};
-            var ebo = new int[eboTemplate.Length * (stepCount+1)];
-            
-            
-            var step = 2*Mathf.PI / stepCount;
+            var ebo = new int[eboTemplate.Length * (stepCount + 1)];
+
+
+            var step = 2 * Mathf.PI / stepCount;
 
             float angle = 0;
             var eboIdx = 0;
-            for (int i = 0, vertIdx = 0; i <= stepCount; i+=2, vertIdx += 4, angle = i*step)
+            for (int i = 0, vertIdx = 0; i <= stepCount; i += 2, vertIdx += 4, angle = i * step)
             {
-                var vecF = CalcPoint(i*step, radius);
-                var vecS = CalcPoint((i+1)*step, radius);
-                var vecT = CalcPoint((i+2)*step, radius);
+                var vecF = CalcPoint(i * step, radius);
+                var vecS = CalcPoint((i + 1) * step, radius);
+                var vecT = CalcPoint((i + 2) * step, radius);
 
                 vertices[vertIdx] = vecF;
-                vertices[vertIdx+1] = vecS;
-                vertices[vertIdx+2] = center;
-                vertices[vertIdx+3] = vecT;
-                
+                vertices[vertIdx + 1] = vecS;
+                vertices[vertIdx + 2] = center;
+                vertices[vertIdx + 3] = vecT;
+
                 // 0F--1S--3T
                 //  \  |  /
                 //   \ | /
                 //    2C
-                for (var k = 0; k < eboTemplate.Length; k++) ebo[eboIdx+ k] = vertIdx + eboTemplate[k];
+                for (var k = 0; k < eboTemplate.Length; k++) ebo[eboIdx + k] = vertIdx + eboTemplate[k];
 
                 eboIdx += eboTemplate.Length;
             }
-            
+
             for (var i = 0; i < colors.Length; i++) colors[i] = FillColor;
 
             _mesh.vertices = vertices;
--- Assets/src/Main.cs
+++ Assets/src/Main.cs
@@ -1,157 +1,109 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using src;
-using UnityEditor;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions.Comparers;
-using Random = UnityEngine.Random;
 
-public class Main : MonoBehaviour
+namespace src
 {
-    [SerializeField] private SpriteRenderer backgroundSpriteRenderer;
-    [SerializeField] private Circle circlePrefab;
-    private Camera _mainCamera;
-    private Texture2D _texture;
-    private Vector2Int _textureRes;
-    private float _prevAspect;
-    private Vector3 _defaultTextureSize;
-    Color32[] colors32;
-    private void InitBackground()
+    public class Main : MonoBehaviour
     {
+        private readonly List<Circle> _circles = new List<Circle>();
+        private Color32[] _colors32;
+        private Vector3 _defaultTextureSize;
+        private Camera _mainCamera;
+        private float _prevAspect;
+        private Texture2D _texture;
+        private Vector2Int _textureRes;
+        [SerializeField] private SpriteRenderer backgroundSpriteRenderer;
+        [SerializeField] private Circle circlePrefab;
 
-        _texture = new Texture2D(_textureRes.x, _textureRes.y);
-        var sprite = Sprite.Create(_texture, new Rect(0, 0, _textureRes.x, _textureRes.y), Vector2.zero);
-
-        backgroundSpriteRenderer.sprite = sprite;
-        backgroundSpriteRenderer.transform.position = _mainCamera.ScreenToWorldPoint(new Vector3(0,0,50));
-        
-        _defaultTextureSize = backgroundSpriteRenderer.bounds.size;
-        UpdateTextureSize();
-        
-        colors32 = new Color32[_texture.width*_texture.height];
-    }
-
-    private Vector2 CameraSize()
-    {
-        var cameraHeight = 2 * _mainCamera.orthographicSize;
-        var cameraWidth = cameraHeight * _mainCamera.aspect;
-        
-        return new Vector2(cameraWidth, cameraHeight);
-    }
-    
-    private void UpdateTextureSize()
-    {
-        var size = CameraSize();
-
-        float scaller;
-        if (size.x < size.y)
-            scaller = size.x / _defaultTextureSize.x;
-        else
-            scaller = size.y / _defaultTextureSize.y;
-
-        backgroundSpriteRenderer.transform.localScale = new Vector3(scaller, scaller);
-        
-        backgroundSpriteRenderer.transform.position = _mainCamera.ScreenToWorldPoint(new Vector3(0,0,50));
-    }
-    private void Start()
-    {
-        _textureRes.x = 1280/8;
-        _textureRes.y = 720/8;
-
-
-        _mainCamera = Camera.main;
-        InitBackground();
-
-        _prevAspect = _mainCamera.aspect;
-        
-        _texture.Apply();
-
-        var camSize = CameraSize();
-        for (int i = 0; i < 6; i++)
+        private void InitBackground()
         {
-            _circles.Add(Instantiate(circlePrefab));
-            _circles[i].Radius = Random.Range(0.08f, 0.3f);
-            _circles[i].speed = new Vector2(Random.Range(0f, camSize.x/4), Random.Range(0f, camSize.x/4));
-        }
-    }
+            _texture = new Texture2D(_textureRes.x, _textureRes.y);
+            var sprite = Sprite.Create(_texture, new Rect(0, 0, _textureRes.x, _textureRes.y), Vector2.zero);
 
-    private float ComputeColor(int x, int y, Vector2 center)
-    {
-        var distance = Vector2.Distance(new Vector2(x, y), center);
-//        Mathf.Abs(x-center.x)+Mathf.Abs(y-center.y);
-        return distance;
-    }
+            backgroundSpriteRenderer.sprite = sprite;
+            backgroundSpriteRenderer.transform.position = _mainCamera.ScreenToWorldPoint(new Vector3(0, 0, 50));
 
-    private List<Circle> _circles = new List<Circle>();
-    private void Update()
-    {
-        if (!FloatComparer.AreEqualRelative(_prevAspect, _mainCamera.aspect, 0.0001f))
-        {
+            _defaultTextureSize = backgroundSpriteRenderer.bounds.size;
             UpdateTextureSize();
+
+            _colors32 = new Color32[_texture.width * _texture.height];
+        }
+
+        private Vector2 CameraSize()
+        {
+            var cameraHeight = 2 * _mainCamera.orthographicSize;
+            var cameraWidth = cameraHeight * _mainCamera.aspect;
+
+            return new Vector2(cameraWidth, cameraHeight);
+        }
+
+        private void UpdateTextureSize()
+        {
+            var size = CameraSize();
+
+            float scale;
+            if (size.x < size.y)
+                scale = size.x / _defaultTextureSize.x;
+            else
+                scale = size.y / _defaultTextureSize.y;
+
+            backgroundSpriteRenderer.transform.localScale = new Vector3(scale, scale);
+
+            backgroundSpriteRenderer.transform.position = _mainCamera.ScreenToWorldPoint(new Vector3(0, 0, 50));
+        }
+
+        private void Start()
+        {
+            _textureRes.x = 1280 / 8;
+            _textureRes.y = 720 / 8;
+
+
+            _mainCamera = Camera.main;
+            InitBackground();
+
             _prevAspect = _mainCamera.aspect;
+
+            _texture.Apply();
+
+            GenerateCircles();
         }
-        
-        
-        for (var y = 0; y < _texture.height; y++)
-        for (var x = 0; x < _texture.width; x++)
+
+        private void GenerateCircles()
         {
-            var sum = 0f;
-            foreach (var c in _circles)
+            var camSize = CameraSize();
+            for (var i = 0; i < 6; i++)
             {
-                var val = ComputeColor(x, y, c.TexturePosition);
-                val = c.Radius / val;
-                sum += val;
+                _circles.Add(Instantiate(circlePrefab));
+                _circles[i].Radius = Random.Range(0.08f, 0.3f);
+                _circles[i].speed = new Vector2(Random.Range(0f, camSize.x / 4), Random.Range(0f, camSize.x / 4));
+            }
+        }
+
+        private float ComputeColor(int x, int y, Vector2 center)
+        {
+            var distance = Vector2.Distance(new Vector2(x, y), center);
+            return distance;
+        }
+
+        private void Update()
+        {
+            if (!FloatComparer.AreEqualRelative(_prevAspect, _mainCamera.aspect, 0.0001f))
+            {
+                UpdateTextureSize();
+                _prevAspect = _mainCamera.aspect;
             }
 
-            sum *= 1000*6;
-            if (sum > 255) sum = 255;
-            byte sumb = (byte) sum;
-            colors32[x + y * _texture.width] = new Color32(sumb,sumb,sumb, 255);
+            ProcessInput();
+            UpdateCirclePositions();
+
+            UpdateTexture();
         }
-            
-        _texture.SetPixels32(colors32);
-        _texture.Apply();
 
-        var bottomLeft = _mainCamera.ScreenToWorldPoint(new Vector3(0,0,50));
-        var upperRight = _mainCamera.ScreenToWorldPoint(new Vector3(_mainCamera.pixelWidth,_mainCamera.pixelHeight,50));
-        
-        
-        foreach (var circle in _circles)
+        private void UpdateTexture()
         {
-            var circlePos = circle.transform.position;
-            if (circlePos.x > upperRight.x || circlePos.x < bottomLeft.x)
-            {
-                circle.speed.x *= -1;
-            }
-            
-            if (circlePos.y > upperRight.y || circlePos.y < bottomLeft.y)
-            {
-                circle.speed.y *= -1;
-            }
-
-            circlePos += Time.deltaTime * circle.speed;
-            circle.transform.position = circlePos;
-            circle.TexturePosition = WorldToTexturePoint(circlePos);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            var pz = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            pz.z = 0;
-
-            var circle = Instantiate(circlePrefab);
-            circle.transform.position = pz;
-            circle.Radius = 0.1f;
-//            circle.Create(0.1f);
-            _circles.Add(circle);
-            
-            var pos = WorldToTexturePoint(_mainCamera.ScreenToWorldPoint(Input.mousePosition));
-            circle.TexturePosition = pos;
-            
-            
-            for (var y = 0; y < _texture.height; y++)
-            for (var x = 0; x < _texture.width; x++)
+            for (int y = 0, i = 0; y < _texture.height; y++)
+            for (var x = 0; x < _texture.width; x++, i++)
             {
                 var sum = 0f;
                 foreach (var c in _circles)
@@ -161,43 +113,68 @@ public class Main : MonoBehaviour
                     sum += val;
                 }
 
-                sum *= 1000*10;
+                sum *= 6000;
                 if (sum > 255) sum = 255;
-                byte sumb = (byte) sum;
-                colors32[x + y * _texture.width] = new Color32(sumb,sumb,sumb, 255);
+                var sumB = (byte) sum;
+                _colors32[i] = new Color32(sumB, sumB, sumB, 255);
             }
-            
-            _texture.SetPixels32(colors32);
+
+            _texture.SetPixels32(_colors32);
             _texture.Apply();
         }
-    }
 
-    private void TestInput()
-    {
-        var pos = WorldToTexturePoint(_mainCamera.ScreenToWorldPoint(Input.mousePosition));
-        _texture.SetPixel((int)pos.x, (int)pos.y, Color.red);
-        _texture.Apply();
-    }
+        private void ProcessInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0)) CreateStaticCircle();
+        }
 
-    private Vector3 ScreenToTexturePoint(Vector3 pos)
-    {
-        throw new Exception("Doesn't work with not default aspect");
-        var scaller = new Vector2(_mainCamera.pixelWidth/(float)_textureRes.x, _mainCamera.pixelHeight/(float)_textureRes.y);
-        return new Vector3(pos.x / scaller.x, pos.y/scaller.y);
-    }
+        private void UpdateCirclePositions()
+        {
+            var bottomLeft = _mainCamera.ScreenToWorldPoint(new Vector3(0, 0, 50));
+            var upperRight =
+                _mainCamera.ScreenToWorldPoint(new Vector3(_mainCamera.pixelWidth, _mainCamera.pixelHeight, 50));
 
-    private Vector3 WorldToTexturePoint(Vector3 pos)
-    {
-        var bounds = backgroundSpriteRenderer.bounds;
-        var textureX = bounds.size.x;
-        var textureY = bounds.size.y;
+            foreach (var circle in _circles)
+            {
+                var circlePos = circle.transform.position;
+                if (circlePos.x > upperRight.x || circlePos.x < bottomLeft.x) circle.speed.x *= -1;
 
-        var camSize = CameraSize();
-        
-        // x          - (pos+shift)
-        // resolution - texsize
-        var relation = new Vector2(_textureRes.x/textureX, _textureRes.y/textureY);
-        var camPosition = _mainCamera.transform.position;
-        return new Vector3((pos.x + camSize.x/2 - camPosition.x)* relation.x, (pos.y+camSize.y/2- camPosition.y)*relation.y);
+                if (circlePos.y > upperRight.y || circlePos.y < bottomLeft.y) circle.speed.y *= -1;
+
+                circlePos += Time.deltaTime * circle.speed;
+                circle.transform.position = circlePos;
+                circle.TexturePosition = WorldToTexturePoint(circlePos);
+            }
+        }
+
+        private void CreateStaticCircle()
+        {
+            var pz = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            pz.z = 0;
+
+            var circle = Instantiate(circlePrefab);
+            circle.transform.position = pz;
+            circle.Radius = 0.1f;
+            _circles.Add(circle);
+
+            var pos = WorldToTexturePoint(_mainCamera.ScreenToWorldPoint(Input.mousePosition));
+            circle.TexturePosition = pos;
+        }
+
+        private Vector3 WorldToTexturePoint(Vector3 pos)
+        {
+            var bounds = backgroundSpriteRenderer.bounds;
+            var textureX = bounds.size.x;
+            var textureY = bounds.size.y;
+
+            var camSize = CameraSize();
+
+            // x          - (pos+shift)
+            // resolution - texture size
+            var relation = new Vector2(_textureRes.x / textureX, _textureRes.y / textureY);
+            var camPosition = _mainCamera.transform.position;
+            return new Vector3((pos.x + camSize.x / 2 - camPosition.x) * relation.x,
+                (pos.y + camSize.y / 2 - camPosition.y) * relation.y);
+        }
     }
 }
